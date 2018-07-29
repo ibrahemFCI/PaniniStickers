d146ecac761872aa89c8f8180c1171a31c6cf2f8
ibrahemFCI <ibrahem.robil1994@gmail.com>
2018-07-29 21:18:58 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     database:"mongodb://localhost/paninistickersdb",
-    database_online:"mongodb://ibrahemrobil:Admin!123@ds147180.mlab.com:47180/nodesdb",
+    database_online:"DatabaseNameAndAccount",
     secrit:"mysct"
 }
